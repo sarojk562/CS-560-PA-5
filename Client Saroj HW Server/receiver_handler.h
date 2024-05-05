bd4b920df73eadab4f57da866190af0d3ad652bf
Saroj Kaashyap Pokkula <sarojk@incentsoft.com>
2024-05-05 11:45:16 -0700

    Keeping only one client

--- Client Saroj HW Server/receiver_handler.h
+++
@@ -1,6 +0,0 @@
-#ifndef RECEIVER_HANDLER_H
-#define RECEIVER_HANDLER_H
-// Function to start the receiving handler thread
-void *receiver_handler(void *arg);
-
-#endif
--- Client Saroj HW Server/sender_handler.h
+++
@@ -1,7 +0,0 @@
-#ifndef SENDER_HANDLER_H
-#define SENDER_HANDLER_H
-
-// Function to start the sending handler thread
-void *sender_handler(void *arg);
-
-#endif
